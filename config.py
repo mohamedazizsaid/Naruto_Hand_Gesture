1c2cdc602ad00605ca8aee5379570ae9f0969fa0
aziz <saidazizz132@gmail.com>
2026-02-03 17:22:20 +0100

    config

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Configuration settings for the Naruto Jutsu Trainer."""
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+CAMERA_INDEX = 0
+
+COLORS = {
+    'primary': (255, 165, 0),
+    'secondary': (0, 255, 255),
+    'success': (0, 255, 0),
+    'warning': (0, 165, 255),
+    'danger': (0, 0, 255),
+    'info': (255, 255, 0),
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    'chakra_blue': (255, 191, 0),
+    'chakra_orange': (0, 140, 255),
+    'shadow': (50, 50, 50),
+}
